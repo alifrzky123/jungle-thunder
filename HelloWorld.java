1897c439cc5f9d1cbe56541965ab343c0f376a3b
alifrizky <alifrzky78@gmail.com>
2019-12-05 12:38:07 +0700

    Menambah merk mobil

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,20 @@
 public class HelloWorld{
     public static void main(String[] args){
         System.out.println("Hello World");
+        mobil bmw = new mobil(); //object mobil bmw
+        bmw.merk = "BMW i3000";
+        bmw.maju();
+
+        mobil fordShelby = new mobil();
+        fordShelby.merk = "Ford Mustang";
+        fordShelby.maju();
+    }
+}
+
+class mobil{
+    String merk;
+
+    public void maju(){
+        System.out.println("Mobil "+this.merk+" Maju! ");
     }
 }
